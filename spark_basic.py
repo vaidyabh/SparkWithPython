d4e43d0d43e9fb5a433bd30a00fcd4c8fdb997a4
Abhinav Vaidya <abhinavv1@birlasoft.com>
2020-08-04 17:34:20 +0530

    Adding the basic Pyspark function file

--- spark_basic.py
+++ spark_basic.py
@@ -1,3 +1,24 @@
+'''
+    This python file consists of basic pyspark functions with examples
+    Functions in the files
+    1) Lower case data                                      2) Case statement in Python
+    3) Converting to another data type                      4) Distinct records using Spark
+'''
+
+
 # convert the data to the lower case
     from pyspark.sql import functions as funct
     df.withColumn('new_column', funct.lower(funct.col('column_name')))
+    
+    
+# Use of when in Pyspark
+    from pyspark.sql.functions import when
+    df2.withColumn('is_abnormal', when(df2.is_abnormal == 'n', 1).when(df2.is_abnormal == 'a', 2).when(df2.is_abnormal == 'l', 3).when(df2.is_abnormal == 'p', 4))
+
+    
+# Cast string column to int
+    df3.withColumn('is_abnormal', df3.is_abnormal.cast('Int'))
+
+    
+# Select Distinct Records
+    df3.select('column_name').distinct()
